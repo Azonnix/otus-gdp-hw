bac2857f7c80cf7987df048a7ca6bc92286a4708
Azonnix <a.alekseev@lympro.ru>
2023-12-12 16:31:53 +0300

    add parallel stages

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,9 @@
 package hw06pipelineexecution
 
+import (
+	"sync"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +13,51 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var wg sync.WaitGroup
+	var cc []<-chan interface{}
+
+	for i := range in {
+		i := i
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ic := make(chan interface{}, 1)
+			ic <- i
+			close(ic)
+			inc := stages[0](ic)
+			for _, stage := range stages[1:] {
+				inc = stage(inc)
+			}
+			cc = append(cc, inc)
+		}()
+	}
+
+	wg.Wait()
+
+	return merge(cc...)
+}
+
+func merge(cs ...In) Out {
+	var wg sync.WaitGroup
+	out := make(chan interface{})
+
+	output := func(c In) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(cs))
+
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
