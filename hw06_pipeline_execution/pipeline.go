1e76a49e2425a957d7580400b976a551fae39dad
Azonnix <aazonnix@gmail.com>
2024-02-06 15:52:33 +0300

    implement pipeline execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,9 +1,5 @@
 package hw06pipelineexecution
 
-import (
-	"sync"
-)
-
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -13,51 +9,79 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	var wg sync.WaitGroup
-	var cc []<-chan interface{}
+	out := in
 
-	for i := range in {
-		i := i
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			ic := make(chan interface{}, 1)
-			ic <- i
-			close(ic)
-			inc := stages[0](ic)
-			for _, stage := range stages[1:] {
-				inc = stage(inc)
-			}
-			cc = append(cc, inc)
-		}()
+	for _, stage := range stages {
+		out = stage(out)
 	}
 
-	wg.Wait()
-
-	return merge(cc...)
-}
-
-func merge(cs ...In) Out {
-	var wg sync.WaitGroup
-	out := make(chan interface{})
-
-	output := func(c In) {
-		for n := range c {
-			out <- n
-		}
-		wg.Done()
-	}
-
-	wg.Add(len(cs))
-
-	for _, c := range cs {
-		go output(c)
-	}
+	result := make(Bi)
 
 	go func() {
-		wg.Wait()
-		close(out)
+		defer close(result)
+
+		for {
+			select {
+			case <-done:
+				return
+			case pipelineOut, ok := <-out:
+				if !ok {
+					return
+				}
+
+				result <- pipelineOut
+			}
+		}
 	}()
 
-	return out
+	return result
 }
+
+// var wg sync.WaitGroup
+// var cc []<-chan interface{}
+
+// for i := range in {
+// 	i := i
+// 	wg.Add(1)
+// 	go func() {
+// 		defer wg.Done()
+// 		ic := make(chan interface{}, 1)
+// 		ic <- i
+// 		close(ic)
+// 		inc := stages[0](ic)
+// 		for _, stage := range stages[1:] {
+// 			inc = stage(inc)
+// 		}
+// 		cc = append(cc, inc)
+// 	}()
+// }
+
+// wg.Wait()
+
+// return merge(cc...)
+// }
+
+// func merge(cs ...In) Out {
+// 	var wg sync.WaitGroup
+// 	out := make(chan interface{})
+
+// 	output := func(c In) {
+// 		for n := range c {
+// 			out <- n
+// 		}
+// 		wg.Done()
+// 	}
+
+// 	wg.Add(len(cs))
+
+// 	for _, c := range cs {
+// 		go output(c)
+// 	}
+
+// 	go func() {
+// 		wg.Wait()
+// 		close(out)
+// 	}()
+
+// 	return out
+// }
