2920f16e23f72688ada5410f1b6c042424c307a9
Azonnix <aazonnix@gmail.com>
2024-02-07 23:18:50 +0300

    add test for unsupported file

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -23,8 +23,22 @@ func TestCopy(t *testing.T) {
 		expected: ErrOffsetExceedsFileSize,
 	}
 
+	testNoFile := errorTestCase{
+		from:     "/dev/urandom",
+		to:       "out_offset0_limit0.txt",
+		offset:   0,
+		limit:    0,
+		expected: ErrUnsupportedFile,
+	}
+
 	t.Run("Error offset exceeds file size", func(t *testing.T) {
 		err := Copy(testCaseOffsetExceeds.from, testCaseOffsetExceeds.to, testCaseOffsetExceeds.offset, testCaseOffsetExceeds.limit)
-		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+		require.ErrorIs(t, err, testCaseOffsetExceeds.expected)
+	})
+
+	t.Run("Error not found", func(t *testing.T) {
+		err := Copy(testNoFile.from, testNoFile.to, testNoFile.offset, testNoFile.limit)
+
+		require.ErrorIs(t, err, testNoFile.expected)
 	})
 }
