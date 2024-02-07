9cc47995434d1c27e841c5a4aaebfd9904736379
Azonnix <aazonnix@gmail.com>
2024-02-07 22:28:01 +0300

    implement copy

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,39 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromStat, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if offset > fromStat.Size() {
+		return errors.New("invalid offset")
+	}
+
+	if limit == 0 || limit > fromStat.Size() {
+		limit = fromStat.Size()
+	}
+
+	fromFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	_, err = fromFile.Seek(offset, io.SeekCurrent)
+	if err != nil {
+		return err
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	reader := io.LimitReader(fromFile, limit)
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(reader)
+	io.Copy(toFile, barReader)
+	bar.Finish()
+
 	return nil
 }
